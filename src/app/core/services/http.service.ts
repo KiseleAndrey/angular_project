410ed1668511e9a4be38c505d7a156236c1b2a92
Kiselev Andrey <Kiselev.andrey@mail.ru>
2017-07-24 16:32:58 +0400

    refactoring

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,6 +1,12 @@
 /**
  * 
  */
-export class HttpService{
+import { Headers, Http } from '@angular/http';
+import { Injectable }    from '@angular/core';
+@Injectable()
+export class HttpService {
+    constructor(http: Http) { }
 
+    get() { }
+    post() { }
 }
