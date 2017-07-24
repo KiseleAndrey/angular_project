1b2edd66d1c219c3f5c99394976c77050e54d702
Kiselev Andrey <Kiselev.andrey@mail.ru>
2017-07-24 16:13:25 +0400

    add shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+/***
+ * 
+ */
+
+import {NgModule} from '@angular/core'
+import {HttpModule} from '@angular/http'
+
+@NgModule({
+    imports: [
+        HttpModule
+    ]
+})
+
+export class SharedModule {
+
+}
